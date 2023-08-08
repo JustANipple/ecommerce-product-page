4b51e8a441292c432a5f8f0770e99606f1e3d3b7
JustANipple <prandisamuel99@gmail.com>
2023-08-08 16:11:11 +0200

    modified:   src/App.module.css 	modified:   src/components/cart/Cart.jsx 	modified:   src/components/cart/Cart.module.css

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react"
 import styles from "./Cart.module.css"
 
 const Cart = () => {
 
+    const [cart, setCart] = useState(false);
+
 
     function handleClick() {
-
+        setCart(!cart);
     }
 
     function CartItem() {
         return (
-            <></>
+            <div className={styles.item}>
+                <img 
+                    src="image-product-1-thumbnail.jpg"
+                    alt=""
+                    aria-hidden="true"
+                    className={styles.item_picture}
+                />
+                <div className={styles.item_product}>
+                    <p className={styles.product_name}>
+                        Fall Limited Edition Sneakers
+                    </p>
+                    <p className={styles.product_price}>
+                        $125.00 x 3 <span className={styles.price_total}>$375.00</span>
+                    </p>
+                </div>
+                <button className={styles.item_delete}>
+                    <img 
+                        src="icon-delete.svg" 
+                        alt="delete icon"
+                    />
+                </button>
+            </div>
         )
     }
     
@@ -22,15 +46,15 @@ const Cart = () => {
                 <img 
                     src="icon-cart.svg"
                     alt="cart icon"
-                    className={styles.cart_icon}
+                    className={cart ? styles.darken_icon : ""}
                 />
                 <div className={styles.cart_quantity}>3</div>
             </button>
-            <div className={styles.cart}>
-                <p>Cart</p>
-                <hr />
+            <div className={`${styles.cart} ${cart ? styles.cart_open : ""}`}>
+                <p className={styles.title}>Cart</p>
+                <hr className={styles.divisor}/>
                 <CartItem />
-                <button>Checkout</button>
+                <button className={styles.cart_checkout}>Checkout</button>
             </div>
         </>
     )
